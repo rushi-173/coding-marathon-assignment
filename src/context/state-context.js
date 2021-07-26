178d9507ec01cd012d69e4e946c124ef283787ad
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-07-26 12:24:33 +0530

    adding for submission🚀

--- src/context/state-context.js
+++ src/context/state-context.js
@@ -12,18 +12,18 @@ export const StateContextProvider = ({ children }) => {
     let filteredData = [...data];
   
     if (state.filters.sortByBrand.length !== 0) {
-      filteredData = filteredData.filter((data) =>
-        state.filters.sortByBrand.includes(data.brand)
+      filteredData = filteredData.filter((product) =>
+        state.filters.sortByBrand.includes(product.brand)
       );
     }
     if (state.filters.sortBySize.length !== 0) {
-      filteredData = filteredData.filter((data) =>
-        state.filters.sortBySize.includes(data.size)
+      filteredData = filteredData.filter((product) =>
+        state.filters.sortBySize.includes(product.size)
       );
     }
     if (state.filters.sortByIdealFor.length !== 0) {
-      filteredData = filteredData.filter((data) =>
-        state.filters.sortByIdealFor.includes(data.ideal)
+      filteredData = filteredData.filter((product) =>
+        state.filters.sortByIdealFor.includes(product.ideal)
       );
     }
   
